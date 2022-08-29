b563a13a13774b7dfbefcbbb8a6b04c729d58303
manojkumar0208 <rvmanojkumar02@gmail.com>
2022-08-29 10:01:32 +0530

    yyyyy

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,24 @@
+package org.emp;
+
+public class Employee {
+	public void greensOmr(String Name, long No) {
+		System.out.println(Name+"\n"+No);		
+	}
+	public void greensOmr(String name, String name1) {
+		System.out.println(name+"\n"+name1);
+	}
+	private void greensOmr(long No , String Name) {
+		System.out.println(No+"\n"+Name);
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.greensOmr("manojkumar", 464646546);
+		e.greensOmr(556565, "kumar");
+		e.greensOmr("manojkumar", "Manojkumarrr");
+	}
+
+
+
+}
+
